299e582ab61fd9725e3a911f00ec0875a18dbdf3
Samuel Eliab Granados Figuera <samuelgranados2801@gmail.com>
2023-12-23 03:09:29 -0300

    enviar videos

--- js/criarVideos.js
+++ js/criarVideos.js
@@ -1,11 +1,17 @@
+import { conectaApi } from "./conectaApi.js";
+
 const formulario = document.querySelector("[data-formulario]");
 
-function criarVideo(evento) {
+async function criarVideo(evento) {
     evento.preventDefault();
     const imagem = document.querySelector("[data-imagem]");
     const url = document.querySelector("[data-url]");
     const titulo = document.querySelector("[data-titulo]");
     const descricao = Math.floor(Math.random() * 10).toString();
+
+    await conectaApi.criaVideos(titulo, descricao, url, imagem);
+
+    window.location.href = "../pages/envio-concluido.html";
 }
 
 formulario.addEventListener("submit", evento => criarVideo(evento));
