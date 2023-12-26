46d32f9feb1e4a9e8b29596a1ac02bcf763c7a40
Samuel Eliab Granados Figuera <samuelgranados2801@gmail.com>
2023-12-25 23:10:39 -0300

    carregamento da lista

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -26,7 +26,7 @@ async function criaVideos(titulo, descricao, url, imagem) {
 }
 
 async function buscaVideo() {
-    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
     const conexaoConvertida = conexao.json();
 
     return conexaoConvertida;
